4f4beb8bb95ee6b768d481bc2d849632785aab52
elenmagalhaes <fromeleen@gmail.com>
2021-08-11 06:24:56 -0300

    Foi necessária uma reformulação no Podfile e em algumas configs do projeto para que o app rodasse no simulador de ios, isto porque a versão do RN utilizada está com conflito com a versão utilizada para o XCode. Além disso, adicionado o método de listar e de dar likes no repositórios, assim como atualizando seu componente em tela.

--- jest.config.js
+++ jest.config.js
@@ -91,7 +91,7 @@ module.exports = {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  preset: '@testing-library/react-native',
+  preset: "react-native",
 
   // Run tests from one or more projects
   // projects: undefined,
@@ -174,9 +174,9 @@ module.exports = {
   // },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  transformIgnorePatterns: [
+    "/node_modules/(?!(@react-native|react-native)/).*/"
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
